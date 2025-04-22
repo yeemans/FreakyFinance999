726e51495b8e96a4981a3693e1db08e573a04a77
yeemans <arthur.roth.07@gmail.com>
2025-04-22 13:59:41 -0700

    added ability to save dates in start and end date buttons

--- createSheet.py
+++ createSheet.py
@@ -5,29 +5,50 @@ import tkcalendar
 
     
 class MyApp:
-    def setStartDate(self):
-        for widget in self.root.winfo_children():
-            if widget is self.startCalendarWidget: widget.destroy()
-
-        self.startCalendarWidget = tkcalendar.Calendar(self.root)
-        self.startCalendarWidget.pack()
-        # replace the show start calendar button with a button to save the date
+    def get_start_date(self):
+        if self.start_calendar_widget: self.start_calendar_widget.destroy()
+        self.start_calendar_widget = tkcalendar.Calendar(self.root)
+        self.start_calendar_widget.pack()
         
+        # replace the show start calendar button with a button to save the date
+        self.start_date_button.destroy()
+        self.start_date_button = tk.Button(self.frame, text=f"Save date", command=self.save_start_date)
+        self.start_date_button.grid(row=1, column=1, columnspan=1, pady=10)
 
-    def setEndDate(self):
-        for widget in self.root.winfo_children():
-            if widget is self.endCalendarWidget: widget.destroy()
+    def save_start_date(self):
+        self.start_date = self.start_calendar_widget.get_date()
+        # hide calendar and change save button back to button that shows the date
 
-        self.endCalendarWidget = tkcalendar.Calendar(self.root)
-        self.endCalendarWidget.pack()
+        self.start_date_button = tk.Button(self.frame, text=f"Start Date: {self.start_date}", command=self.get_start_date)
+        self.start_date_button.grid(row=1, column=1, columnspan=1, pady=10)
+        self.start_calendar_widget.destroy()
+        self.start_calendar_widget = None
+
+    def get_end_date(self):
+        if self.end_calendar_widget: self.end_calendar_widget.destroy()
+        self.end_calendar_widget = tkcalendar.Calendar(self.root)
+        self.end_calendar_widget.pack()
+        
+        # replace the show end calendar button with a button to save the date
+        self.end_date_button.destroy()
+        self.end_date_button = tk.Button(self.frame, text=f"Save date", command=self.save_end_date)
+        self.end_date_button.grid(row=1, column=2, columnspan=1, pady=10)
+
+    def save_end_date(self):
+        self.end_date = self.end_calendar_widget.get_date()
+        # hide calendar and change save button back to button that shows the date
+
+        self.end_date_button = tk.Button(self.frame, text=f"End Date: {self.end_date}", command=self.get_end_date)
+        self.end_date_button.grid(row=1, column=2, columnspan=1, pady=10)
+        self.end_calendar_widget.destroy()
 
     def __init__(self, root):
         self.root = root
         self.root.title("Create Sheets")
-        self.startCalendarWidget = None
-        self.endCalendarWidget = None
-        self.startDate = None
-        self.endDate = None
+        self.start_calendar_widget = None
+        self.end_calendar_widget = None
+        self.start_date = None
+        self.end_date = None
         
         self.frame = tk.Frame(root)
         self.frame.pack(padx=20, pady=20)
@@ -41,13 +62,15 @@ class MyApp:
         
         # Create the button to add new columns
         self.add_column_button = tk.Button(self.frame, text="Add New Column", command=self.prompt_for_column)
-        self.add_column_button.grid(row=0, column=0, columnspan=1, pady=10)
+        self.add_column_button.grid(row=1, column=0, columnspan=1, pady=10)
+                                                                                
+        self.start_date_button = tk.Button(self.frame, text=f"Start: 4/22/2025", 
+                                           command=self.get_start_date)
+        self.start_date_button.grid(row=1, column=1, columnspan=1, pady=10)
 
-        self.start_date_button = tk.Button(self.frame, text="Set Start Date", command=self.setStartDate)
-        self.start_date_button.grid(row=0, column=1, columnspan=1, pady=10)
-
-        self.end_date_button = tk.Button(self.frame, text="Set End Date", command=self.setEndDate)
-        self.end_date_button.grid(row=0, column=2, columnspan=1, pady=10)
+        self.end_date_button = tk.Button(self.frame, text="End: 5/22/2025", 
+                                         command=self.get_end_date)
+        self.end_date_button.grid(row=1, column=2, columnspan=1, pady=10)
         # Create the initial table
         self.create_table()
 
@@ -79,11 +102,11 @@ class MyApp:
         for col, expense_list in enumerate(self.expenses_by_category.items()):
             category = expense_list[0]
             col_label = tk.Label(self.frame, text=category, relief="solid", width=15, height=2)
-            col_label.grid(row=3, column=col, padx=5, pady=5)
+            col_label.grid(row=4, column=col, padx=5, pady=5)
 
         # display expenses and "New Expense" buttons
         for col, expense_category in enumerate(self.expenses_by_category.items()):
-            ROW_PADDING = 4
+            ROW_PADDING = 5
             category_name, expense_list = expense_category
             print([category_name, col])
 
